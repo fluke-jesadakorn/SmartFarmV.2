0f8498950167845561582fccbf1b873c28f9663f
JESSADAKORN KIRTNU (เจษฎากร เกิดหนู) <5835512119@email.psu.ac.th>
2020-01-14 16:39:58 +0000

    autopull

--- autoPull.js
+++ autoPull.js
@@ -1,27 +1,24 @@
 const cp = require('child_process');
-(async function () {
-    await cp.exec('pm2 delete node server', (err, stdout, stderr) => {
+
+(function () {
+    cp.exec('pm2 delete node server', (err, stdout, stderr) => {
         if (err) {
-            console.log(err)
-            cp.exec('pm2 start node /root/SmartFarmV.2/server.js', (err, stdout, stderr) => {
-                if (err) {
-                    console.log(err)
-        
-                } else {
-                    console.log(stdout)
-                }
-            })
+            console.log(err);
+            startServer();
 
         } else {
-            console.log(stdout)
-            cp.exec('pm2 start node /root/SmartFarmV.2/server.js', (err, stdout, stderr) => {
-                if (err) {
-                    console.log(err)
-        
-                } else {
-                    console.log(stdout)
-                }
-            })
+            console.log(stdout);
+            startServer();
         }
     });
 }())
+
+startServer = () => {
+    cp.exec('pm2 start node /root/SmartFarmV.2/server.js', (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(stdout);
+        }
+    })
+}
