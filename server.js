a13351d455d8f419eb2f5eb293e2d74dca1f9219
JESSADAKORN KIRTNU (เจษฎากร เกิดหนู) <5835512119@email.psu.ac.th>
2020-01-14 09:10:14 +0000

    1. test https

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-const express = require('express')
-const next = require('next')
+// const express = require('express')
+// const next = require('next')
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+// const port = parseInt(process.env.PORT, 10) || 3000
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express()
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+// app.prepare().then(() => {
+//   const server = express()
+//   server.all('*', (req, res) => {
+//     return handle(req, res)
+//   })
 
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+//   server.listen(port, err => {
+//     if (err) throw err
+//     console.log(`> Ready on http://localhost:${port}`)
+    
+//   })
+// })
+
+var https = require('https');
+var fs = require('fs');
+var express = require('express');
+
+var https_options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/private.key"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/your_domain_name.crt"),
+  ca: fs.readFileSync('/etc/letsencrypt/live/nbiot.werapun.com-0001/CA_root.crt')
+};
+
+var app = express();
+var port = process.env.PORT || 443;
+var server = https.createServer( https_options , app );
+
+server.listen( port, function () {
+    console.log( 'Hello IREALLYHOST listening on port ' + server.address().port );
+});
