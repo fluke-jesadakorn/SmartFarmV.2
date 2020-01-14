763dfc303f1637b40a80b9b69663c65114d3017a
JESSADAKORN KIRTNU (เจษฎากร เกิดหนู) <5835512119@email.psu.ac.th>
2020-01-14 16:31:52 +0000

    auto pull

--- autoPull.js
+++ autoPull.js
@@ -1,19 +1,20 @@
 const cp = require('child_process');
-(async function () {
-    await cp.exec('pm2 delete node server', (err, stdout, stderr) => {
+(function () {
+    cp.exec('pm2 delete node server', (err, stdout, stderr) => {
         if (err) {
             console.log(err)
-            
+
         } else {
             console.log(stdout)
         }
+    }, () => {
+        cp.exec('pm2 start node /root/SmartFarmV.2/server.js', (err, stdout, stderr) => {
+            if (err) {
+                console.log(err)
+
+            } else {
+                console.log(stdout)
+            }
+        })
     });
-    await cp.exec('pm2 start node /root/SmartFarmV.2/server.js', (err, stdout, stderr) => {
-        if (err) {
-            console.log(err)
-            
-        } else {
-            console.log(stdout)
-        }
-    })
 }())
