0d2beac74b19c97784b4bbfa6e13bf76399dcef6
JESSADAKORN KIRTNU (เจษฎากร เกิดหนู) <5835512119@email.psu.ac.th>
2020-01-14 17:53:56 +0000

    push

--- server.js
+++ server.js
@@ -18,30 +18,28 @@
 
 //   })
 // })
+const express = require('express');
+const next = require('next');
+// const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const https = require('https');
+const fs = require('fs');
+const appSSL = express();
+const appNonSSL = express();
+const portSSL = process.env.PORT || 443;
 
-var https = require('https');
-var fs = require('fs');
-var express = require('express');
-
-var https_options = {
+let https_options = {
   key: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/privkey.pem", "utf8"),
   cert: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/cert.pem", "utf8"),
   ca: fs.readFileSync('/etc/letsencrypt/live/nbiot.werapun.com-0001/chain.pem', "utf8")
 };
 
-var appSSL = express();
-var appNonSSL = express();
-var port = process.env.PORT || 443;
-var server = https.createServer(https_options, appSSL);
+let serverSSL = https.createServer(https_options, appSSL);
 
 appNonSSL.get("*", (req, res) => {
   res.status(301).redirect('https://nbiot.werapun.com');
-
 })
-
-appNonSSL.post("*"), (req, res)=> {
-  console.log("Pulled");
-}
 
 appNonSSL.listen(80, () => {
   console.log('http ready for redirect to https')
@@ -51,6 +49,13 @@ appSSL.get('/', (req, res) => {
   res.end("successfull SSL")
 })
 
-server.listen(port, function () {
+app.prepare().then(() => {
+  const service = express()
+  service.all('*', (req, res) => {
+    return handle(req, res)
+  })
+})
+
+serverSSL.listen(portSSL, function () {
   console.log('Hello IREALLYHOST listening on port ' + server.address().port);
-});
+})
