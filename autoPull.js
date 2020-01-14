eabb760549a41a0d41f7e04ac7d723109bd6c4ea
JESSADAKORN KIRTNU (เจษฎากร เกิดหนู) <5835512119@email.psu.ac.th>
2020-01-14 17:13:10 +0000

    auto pull

--- autoPull.js
+++ autoPull.js
@@ -2,9 +2,11 @@ const cp = require('child_process');
 const express = require('express');
 const app = express();
 const port = 90;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser())
 app.post('/onPush',(req, res)=>{
-    console.log(req);
+    console.log(req.body);
     res.status(200).end("Thank You For Sending");
 })
 app.listen(port,()=>{
