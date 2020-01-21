41dc8fa69bb3a9b025ef8e48eaf048172ece4075
Fluke <jesadakorn.kirtnu@gmail.com>
2020-01-22 00:46:27 +0700

    Fix LinServer

--- backend/globalHttpsConf.js
+++ backend/globalHttpsConf.js
@@ -2,13 +2,6 @@ const { createServer } = require('https');
 const fs = require('fs');
 
 module.exports = (PORT, appFromExpress, serverType) => {
-    createServer(https_options, appFromExpress, (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-        console.log(`${serverType} Server Running On Port ${PORT}`);
-    })
-        .listen(PORT, err => console.error(err))
-
     const https_options = {
         key: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/privkey.pem", "utf8"),
         cert: fs.readFileSync("/etc/letsencrypt/live/nbiot.werapun.com-0001/cert.pem", "utf8"),
