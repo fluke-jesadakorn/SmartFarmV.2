e18bf7430463bfd2a59f846ae2bae3a59e91b5c6
Fluke <jesadakorn.kirtnu@gmail.com>
2020-02-10 15:56:51 +0700

    fix func Timer

--- backend/NBServ.js
+++ backend/NBServ.js
@@ -47,7 +47,7 @@ const waterOnOff = (OnOff) => {
 	let ack = new Buffer("oo0"+OnOff.toString())
 	if(NBIoT.NbIP !== null){
 		server.send(ack, 0, ack.length, NBIoT.NBPort, NBIoT.NbIP, function (err, bytes) {
-			console.log("sent ACK : " + ack.toString() + 'to NBIoT');
+			console.log("sent ACK : " + ack.toString() + ' to NBIoT');
 		})
 	}else{
 		console.log('Please Wait For NBIoT Connected First');
